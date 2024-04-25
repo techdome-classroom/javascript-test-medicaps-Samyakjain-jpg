4776e550ae7cd42bdf8d5f10b6a64f1f91411737
Samyak Jain <samyakj163@gmail.com>
2024-04-25 14:08:53 +0530

    Done the coding question

--- program2.js
+++ program2.js
@@ -1,7 +1,22 @@
 function smallestMissingPositiveInteger(nums) {
 
-  // Implement the function smallest_missing_positive_integer
+nums = nums.filter(num => num > 0);
 
+
+nums.sort((a, b) => a - b);
+
+let smallest = 1;
+
+
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] === smallest) {
+    smallest++;
+  } else if (nums[i] > smallest) {
+    break;
+  }
+}
+
+return smallest;
 }
 
 module.exports = smallestMissingPositiveInteger;
